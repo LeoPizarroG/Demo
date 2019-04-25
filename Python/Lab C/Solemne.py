8862ef887b0c5df43efddddff83efb59dfee9006
Leonardo Pizarro Gomez <leo.pizarro.gomez@gmail.com>
2019-04-25 13:40:46 -0400

    Eliminar Archivos que no correspondian en la rama

--- Python/Lab C/Solemne.py
+++
@@ -1,9 +0,0 @@
-n = int (input("Ingrese un Natural"))
-caracter = str(input("Ingrese un Caracter"))
-cont = 0
-contdos = n
-caracter2 = caracter
-while cont <= n:
-    print (caracter)
-    caracter = caracter + caracter2
-
